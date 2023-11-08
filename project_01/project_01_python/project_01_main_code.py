6e1e2c22ab28973dfbfdd3a19a713d442614eea5
Your Name <debian@beaglebone.localdomain>
2023-11-08 01:58:17 +0000

    commiting 11/7

---
+++ project_01/project_01_python/project_01_main_code.py
@@ -0,0 +1,89 @@
+"""
+--------------------------------------------------------------------------
+Project 1 
+--------------------------------------------------------------------------
+License:   
+Copyright 2023 - Paula Ortega Gimenez
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this 
+list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, 
+this list of conditions and the following disclaimer in the documentation 
+and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors 
+may be used to endorse or promote products derived from this software without 
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+
+Use the following hardware components to make this project:  
+  - Button
+  - Servo
+
+Requirements:
+  - Hardware:
+    - 
+    - User interaction:
+      - 
+
+Uses:
+  - 
+
+"""
+
+import button        as BUTTON
+import servo         as SERVO
+
+
+# ------------------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------------------
+
+SERVO_LOCK         = 100     # Fully anti-clockwise
+SERVO_UNLOCK       = 0       # Fully clockwise
+
+# ------------------------------------------------------------------------
+# Global variables
+# ------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------
+# Functions / Classes
+# ------------------------------------------------------------------------
+
+class Name():
+    """ Name """
+    
+    # End def
+    
+    def Name()
+        """"""
+
+    # End def
+
+# End class
+
+
+# ------------------------------------------------------------------------
+# Main script
+# ------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+   
+
